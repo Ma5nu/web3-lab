b8cbc3626e514d2d50939a4546b522d213d54084
Ma5nu <ma5nu.100@gmail.com>
2025-08-23 10:55:44 -0300

    Novo exercicio

---
+++ tde2/calculadora-digitos.py
@@ -0,0 +1,6 @@
+'''Solicite ao usuário para inserir um número inteiro e calcule quantos dígitos
+esse número possui.'''
+
+num = int(input("Digite um numero inteiro: "))
+numero_digitos = len(str(abs(num)))
+
